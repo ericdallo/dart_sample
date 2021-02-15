9f43ac5066f0fb414e2f9f3976d7bf0b8832700f
Eric Dallo <ericdallo06@hotmail.com>
2021-02-15 17:24:10 -0300

    Update dart sdk

--- bin/main.dart
+++ bin/main.dart
@@ -1,13 +1,7 @@
-import 'dart:convert';
-import 'dart:io';
-
 // void main() {}
 
 Future<void> main(List<String> args) async {
-  var file = File(args[0]);
-  var lines = utf8.decoder.bind(file.openRead()).transform(LineSplitter());
+  print('Running');
 
-  await for (var line in lines) {
-    if (!line.startsWith('#')) print(line);
-  }
+  print('Done!');
 }
